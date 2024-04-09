f8370fe1dd45c75bc45016753be382dfef7644e8
Saida Taj M R <saidarahaman@Saidas-Mac-mini.local>
2024-04-09 12:34:57 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let type = new Typed('#changing-text' ,{
-    strings:['React Js Expert'],
-    typeSpeed:50,
-    backSpeed:50,
+    strings:['TypeScript','React JS','Angular' ,'Vue JS','Frontend System Design'],
+    typeSpeed:30,
+    backSpeed:30,
     loop:true
 })
